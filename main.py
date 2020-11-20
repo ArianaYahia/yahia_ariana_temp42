6b0d629ea285eb7d41bf55b23713c828c344ca93
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-11-20 15:08:20 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 temp1 = 80
 while True:
-        if temp1 > 60:
-            light.set_pixel_color(4, light.rgb(255, 0, 0 ))
+        if temp1 > 70:
+            light.set_all(light.rgb(255, 0, 0 ))
         elif temp1 > 40:
-            light.set_pixel_color(4, light.rgb(0, 255, 0 ))
+            light.set_all(light.rgb(0, 255, 0 ))
         else: 
-            light.set_pixel_color(4, light.rgb(0, 0, 255 ))
+            light.set_all(light.rgb(0, 0, 255 ))
 
 
 
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 let temp1 = 80
 while (true) {
-    if (temp1 > 60) {
-        light.setPixelColor(4, light.rgb(255, 0, 0))
+    if (temp1 > 70) {
+        light.setAll(light.rgb(255, 0, 0))
     } else if (temp1 > 40) {
-        light.setPixelColor(4, light.rgb(0, 255, 0))
+        light.setAll(light.rgb(0, 255, 0))
     } else {
-        light.setPixelColor(4, light.rgb(0, 0, 255))
+        light.setAll(light.rgb(0, 0, 255))
     }
     
 }
